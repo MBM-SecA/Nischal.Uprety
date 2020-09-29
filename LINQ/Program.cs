9f1633194a8677297147b6e84c04fdf7b7fdfa04
neschal <nischaluprety12@gmail.com>
2020-09-29 11:08:57 +0545

    linq assignment posted

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,56 +1,79 @@
 ﻿using System;
 using System.Linq;
-
+using System.Collections.Generic;
 namespace LINQ
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-             //LINQ-Language Integrated Query
-             int[] numbers = {12,56,34,67,13,25,67,89,39};
+            var countries = Country.GetCountries();
 
-             //Fetch numbers greater than 50
-             //using method syntax-Lambda expression
-             var numbersGreaterThan50 = numbers.Where (x => x>50);
-
-             //using sql query syntax-query expression
-             var numbersGreaterThanFifty = from num in numbers
-                                             where num > 50
-                                             select num;
-
-             string[] names = {"bishnu","ram","ramesh","james","raju","Ramsey"};
-
-             //names having length greater than 3 and starts with r
-             var result1 = names.Where(x => x.Length>3 && x.StartsWith("r"));
-             var result2 = from y in names
-                             where y.Length>3 && y.ToLower().StartsWith("r")
-                             select y;
-             //projections
-             var result5 = from num in numbers
-                            select num*num;
-            //ordering
-            var result6 = from num in numbers
-                            orderby num
-                            select num;
-            //partitioning
-            var result7 = numbers.Skip(5).Take(5);
-            
-            //Quantifier
-            var result8 = numbers.Any(x => x % 2==0);
-            var result9 = numbers.All(x => x % 2==0);
-            var result10 = numbers.Contains(34);
-
-            var result11 = Enumerable.Range(1,1000);
-            var result12 = Enumerable.Repeat("Hello World!",10);
-
-            foreach (var square in result12)
+            //HW - Is there any african country in country collection
+            var africanCountries = countries.Any(x => x.Continent == "Africa");
+            Console.WriteLine(africanCountries);
+            //HW - print first two largest asian country in ascending order 
+            var asianCountriesBySize = from country in countries
+                                       orderby country.Area descending
+                                       select country;
+            foreach (var country in asianCountriesBySize.Take(2))
             {
-                Console.WriteLine(square);
-            }     
-            
+                Console.WriteLine(country.Name);
+            }
 
-                          
+        }
+
+        void general()
+        {
+            // LINQ - Language Integrated Query
+            // LINQ to object 
+            int[] numbers = { 23, 34, 56, 12, 23, 89, 67, 10, 55, 46, 26, 79, 54, 58, 15, 7 };
+            //Fetch numbers which are less than 50
+            var numbersLessThan50 = numbers.Where(x => x < 50);
+            // Using sql query syntax - query expression
+            var numbersLessThanFifty = from num in numbers
+                                       where num < 50
+                                       select num;
+            string[] names = { "nischal", "millie", "pewdiepie", "elon", "gates", "ronaldo", "ram", "Ramsey", "Ramsaili" };
+            var nameswithR = from name in names
+                             where name.ToLower().StartsWith("r") && name.Length > 3
+                             select name;
+            var namesWithR2 = names.Where(name => name.Length > 3 && name.ToLower().StartsWith("r"));
+            // foreach (var item in namesWithR2)
+            // {
+            //     Console.WriteLine(item);
+            // }
+            //Projections
+            var squaredNumbers = numbers.Select(x => x * x);
+            //Ordering
+            var ascendingOrder = from num in numbers
+                                 orderby num
+                                 select num;
+            var decendingOrder = from num in numbers
+                                 orderby num descending
+                                 select num;
+            //Partitioning
+             var skip5 = numbers.Skip(5).Take(5);
+            var takefirst5 = numbers.Take(5);
+            //Quantifiers
+            var anyEvenNumbers = numbers.Any(x => x % 2 == 0);
+            var allEvenNumbers = numbers.All(x => x % 2 == 0);
+            var containsElement = numbers.Contains(23);
+            var repeat = Enumerable.Repeat("Hello World", 10);
+            var range = Enumerable.Range(1, 1000);
+            Console.WriteLine($"{anyEvenNumbers} {allEvenNumbers}");
+            foreach (var number in range)
+            {
+                Console.WriteLine(number);
+            }
+            foreach (var word in repeat)
+            {
+                Console.WriteLine(word);
+            }
+
+            //HW - Is there any african country in country collection
+            //HW - print first two asian country in ascending order that are 
+
         }
     }
 }
