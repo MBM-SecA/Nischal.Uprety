47b0362f0695695301ac9632b93ee54d2e920bb9
neschal <nischaluprety12@gmail.com>
2020-11-24 11:09:55 +0545

    employee management

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,19 +1,30 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 public class EmployeeController : Controller
 {
     public ActionResult Index()
     {
-        
-        Person emp1 = new Person(){ FirstName = "Nischal", Surname = "Uprety", Address = "jhapa", Gender = "M", Salary = 500000 };
-        Person emp2 = new Person(){ FirstName = "Utsav", Surname = "Kuinkel", Address = "Kathmandu", Gender = "M", Salary = 50000 };
-        Person emp3 = new Person(){ FirstName = "Shreedhar", Surname = "Pandey", Address = "Dang", Gender = "M", Salary = 5000 };
-        Person emp4 = new Person(){ FirstName = "Puja", Surname = "Thapa", Address = "jhapa", Gender = "F", Salary = 150000 };
-        Person emp5 = new Person(){ FirstName = "Diya", Surname = "Xetri", Address = "Kathmandu", Gender = "F", Salary = 10000 };
-        Person emp6 = new Person(){ FirstName = "Arshon", Surname = "Niroula", Address = "jhapa", Gender = "M", Salary = 510000 };
-        List<Person> employees = new List<Person>() { emp1,emp2,emp3,emp4,emp5,emp6 };
+        List<Person> employees = Person.GetEmployees();
         return View(employees);
     }
-}
 
+    public ActionResult Detail([FromQuery]string firstName)
+    {
+        var employees = Person.GetEmployees();
+        var employee= employees.FirstOrDefault(x =>x.FirstName == firstName);
+        return View(employee);
+    }
+
+    public ActionResult Add()
+    {
+        return View();
+    }
+    [HttpPost]
+     public ActionResult<string> Add([FromForm]Person person)
+    {
+        return "result saved";
+    }
+
+}
--- EmployeeManagement/Models/Person.cs
+++ EmployeeManagement/Models/Person.cs
@@ -1,10 +1,50 @@
-public class Person 
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+public class Person
 {
-    public string FirstName {get; set;}
-    public string Surname {get; set;}
-    public string Address {get; set;}
-    public string Gender {get; set;}
-    public int Salary {get; set;}
+    [Required]
+    [Display(Name="First Name")]
+    public string FirstName { get; set; }
+    [Required]
+    [Display(Name="Last Name")]
+    public string Surname { get; set; } 
+    public string Address { get; set; }
+    public double? Salary { get; set; }
+    public char? Gender { get; set; } = 'M';
+    
+    public static List<Person> GetEmployees()
+    {
+        Person emp1 = new Person()
+        {
+            FirstName = "Nischal",
+            Surname = "Uprety",
+            Address = "Jhapa",
+            Salary = 100000.0
+        }; 
+        Person emp2 = new Person()
+        {
+            FirstName = "Ram",
+            Surname = "Thapa",
+            Address = "Morang",
+            Salary = 100000.0
+        }; 
+        Person emp3 = new Person()
+        {
+            FirstName = "Utsav",
+            Surname = "Kuinkel",
+            Address = "Kathmandu",
+            Salary = 100000.0
+        }; 
+        Person emp4 = new Person()
+        {
+            FirstName = "Shreedhar",
+            Surname = "Pandey",
+            Address = "Dang",
+            Salary = 100000.0
+        }; 
 
-
+        List<Person> employees = new List<Person> {emp1,emp2,emp3,emp4};
+        return employees;
+    }
 }
